fd8e4a7912638ce10229c02647912143d7ebbce0
Daniel Kawalsky <dkawalsk@usc.edu>
2018-11-25 00:48:22 -0600

    initial commit

---
+++ app/src/main/java/com/pherodev/chatapp/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.pherodev.chatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private Button submitNumberButton;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
